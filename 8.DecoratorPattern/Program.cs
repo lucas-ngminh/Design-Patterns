5b60dff7899cac9461d01e1ae683a22710dd4376
lucas-ngminh <73569714+lucas-ngminh@users.noreply.github.com>
2020-12-25 16:46:18 -0500

    Facade Pattern

--- 8.DecoratorPattern/Program.cs
+++ 8.DecoratorPattern/Program.cs
@@ -2,30 +2,30 @@
 
 namespace _8.DecoratorPattern
 {
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //1. The original system doesn't allow to order customized Pizza
-            Console.WriteLine("Ordering original Pizzas");
-            Pizza pPizza = new Peperonni();
-            Console.WriteLine(pPizza.ToString());
+        //1. The original system doesn't allow to order customized Pizza
+        Console.WriteLine("Ordering original Pizzas");
+        Pizza pPizza = new Peperonni();
+        Console.WriteLine(pPizza.ToString());
 
-            Pizza sPizza = new Sausage();
-            Console.WriteLine(sPizza.ToString());
+        Pizza sPizza = new Sausage();
+        Console.WriteLine(sPizza.ToString());
 
-            Pizza mPizza = new Mushroom();
-            Console.WriteLine(mPizza.ToString());
-            Console.WriteLine();
+        Pizza mPizza = new Mushroom();
+        Console.WriteLine(mPizza.ToString());
+        Console.WriteLine();
 
-            //2. After enhancing the system, can order customized pizza now
-            Console.WriteLine("Ordering customized Pizzas");
-            Pizza myPizza = new Peperonni();
-            Pizza mushroom = new MushroomTopping(myPizza);
-            Pizza extraCheaseMushroom = new ExtraCheeseTopping(mushroom);
-            Console.WriteLine(extraCheaseMushroom.ToString());
-        }
+        //2. After enhancing the system, can order customized pizza now
+        Console.WriteLine("Ordering customized Pizzas");
+        Pizza myPizza = new Peperonni();
+        Pizza mushroom = new MushroomTopping(myPizza);
+        Pizza extraCheaseMushroom = new ExtraCheeseTopping(mushroom);
+        Console.WriteLine(extraCheaseMushroom.ToString());
     }
+}
 
     //1. Existing Classes
     public abstract class Pizza
---
+++ 9.FacadePattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _9.FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderFacade o = new OrderFacade();
+            o.PlaceOrder();
+        }
+    }
+
+    public class Product
+    {
+        public void GetProductDetails()
+        {
+            Console.WriteLine("This is your product details!");
+        }
+    }
+
+    public class Payment
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Proceeded Successfully!");
+        }
+    }
+
+    public class Invoice
+    { 
+        public void SendInvoice()
+        {
+            Console.WriteLine("Invoice Sent Successfully!");
+        }
+    }
+
+    public class OrderFacade
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("---Ordering Proccess Started---");
+            Product p = new Product();
+            p.GetProductDetails();
+            Payment pmt = new Payment();
+            pmt.MakePayment();
+            Invoice inv = new Invoice();
+            inv.SendInvoice();
+            Console.WriteLine("Order Placed Successfully!");
+            Console.WriteLine("---Ordering Proccess Ended---");
+        }
+    }
+}
